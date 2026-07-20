aa8ddc9ac5f2c1d6dbea10986e8997c1fafad8c0
ishaan-cherukuri <ishaan.cherukuri0617@gmail.com>
2026-07-20 10:47:14 -0400

    fixed zoom

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -92,8 +92,8 @@ export default function Window({
         const dw = ev.clientX - resizeRef.current.startX;
         const dh = ev.clientY - resizeRef.current.startY;
         setSize({
-          w: Math.min(Math.max(320, resizeRef.current.startW + dw), Math.floor(window.innerWidth  * 0.70)),
-          h: Math.min(Math.max(240, resizeRef.current.startH + dh), Math.floor(window.innerHeight * 0.70)),
+          w: Math.min(Math.max(320, resizeRef.current.startW + dw), Math.floor(window.innerWidth  * 0.90)),
+          h: Math.min(Math.max(240, resizeRef.current.startH + dh), Math.floor(window.innerHeight * 0.90)),
         });
       };
       const onUp = () => {
